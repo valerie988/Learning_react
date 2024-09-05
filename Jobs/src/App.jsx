77b8d689ca84ab65b93383aa47e2980f5752cb7a
valerie988 <neh16valerie@gmail.com>
2024-09-05 14:06:50 +0100

    first commit

--- Jobs/src/App.jsx
+++ Jobs/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 
-const API_KEY = '9488653d.....'; // YOUR CRACKEDDEVS API KEY HERE
+const API_KEY = '9488653d.....';
 async function crackeddevs() {
   const response = await fetch(
     `https://api.crackeddevs.com/v1/get-jobs`,
     {
       headers: {
-        'api-key': `${API_KEY}`, // API KEY HERE
+        'api-key': `${API_KEY}`,
       },
     }
   );
