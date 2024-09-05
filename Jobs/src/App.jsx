4c20d7a14ad0693be0f7c6c6f58dfa4a9a237aab
valerie988 <neh16valerie@gmail.com>
2024-09-05 13:57:55 +0100

    changes

--- Jobs/src/App.jsx
+++ Jobs/src/App.jsx
@@ -1,20 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(2)
+const API_KEY = '9488653d.....'; // YOUR CRACKEDDEVS API KEY HERE
+async function crackeddevs() {
+  const response = await fetch(
+    `https://api.crackeddevs.com/v1/get-jobs`,
+    {
+      headers: {
+        'api-key': `${API_KEY}`, // API KEY HERE
+      },
+    }
+  );
 
-  return (
-    <>
-  
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      
-    </>
-  )
+  if (response.ok) {
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } else {
+    console.log('HTTP-Error: ' + response.status);
+  }
 }
 
-export default App
+crackeddevs();
+const Jobs = () => {
+  return ( 
+    <>
+    {jobs.map((job) => {
+      const { index } = job
+
+      return <article key={index}>job Data </article>
+    })}
+      <h1>hi</h1>
+      </>
+  )
+}
+export default Jobs
