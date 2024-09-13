4a73f0c4fcf788db838dfcb158ed90f95e892f20
valerie988 <neh16valerie@gmail.com>
2024-09-13 13:55:42 +0100

    change

--- Dashboard/src/components/Header.jsx
+++ Dashboard/src/components/Header.jsx
@@ -1,24 +1,40 @@
-import React from "react";
-import SearchIcon from '@mui/icons-material/Search';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import NightlightRoundIcon from '@mui/icons-material/NightlightRound';
-import DialpadIcon from '@mui/icons-material/Dialpad';
-import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import "../App.css"
+import React, { useEffect, useState } from "react";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import NightlightRoundIcon from "@mui/icons-material/NightlightRound";
+import DialpadIcon from "@mui/icons-material/Dialpad";
+import FormatListBulletedIcon from "@mui/icons-material/FormatListBulleted";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import "../App.css";
+
 const Header = () => {
+  const [themes, setThemes] = useState(false);
+  const change = () => {
+    setThemes(!themes);
+  };
+  useEffect(() => {
+    if (themes) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }, [themes]);
+
   return (
     <div className="header">
       <div className="leftIcon">
-        <SearchIcon className="search"/>
+        <SearchIcon className="search" />
         <input type="text" placeholder="Search" />
       </div>
       <div className="icons">
         <NotificationsIcon />
         <FormatListBulletedIcon />
         <DraftsIcon />
-        <NightlightRoundIcon className="light"/>
-        <img src="" alt="pic" />
+        
+        <div className="light" onClick={change}>
+          {themes ?  <NightlightRoundIcon /> :<LightModeIcon />}
+        </div>
         <DialpadIcon />
       </div>
     </div>
--- Dashboard/src/components/calendar.jsx
+++ Dashboard/src/components/calendar.jsx
@@ -1,26 +1,11 @@
 import React, { useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-import "react-big-calendar/lib/css/react-big-calendar.css";
+import "react-big-calendar/lib/css/react-big-calendar.css"
 
 const localizer = momentLocalizer(moment);
 
 const Fullcalendar = () => {
-  const [events, setEvents] = useState([]);
-
-  const handleSelectorSlot = ({ start, end }) => {
-    const title = window.prompt(`New Event Name`)
-    if(title){
-        setEvents([...events, {start, end, title}])
-    }
-  }
-
-  const handleSelectorEvent = (event) => {
-    const confirmDelete = window.confirm(`Delete event " ${event.title}"? `)
-    if(confirmDelete){
-        setEvents(event.filter((e) => e !== event))
-    }
-}
 
   return (
     <div className="Calendar">
@@ -32,13 +17,11 @@ const Fullcalendar = () => {
         <p className="text">FullCalendar</p>
         <Calendar className="date"
             localizer={localizer}
-            events={events}
+            events={event}
             selectable 
             startAccessor={"<"}
             endAccessor=">"
             style={{ height: 500}}
-            onSelectSlot={handleSelectorSlot}
-            onSelectEvent={handleSelectorEvent}
             defaultView="month"
             popup={true}
         />
